5cb8b12e8c2445e881d62cf02f5e6e350e27a6ff
denizsimsekk <ddenizsimsekk@gmail.com>
2022-10-20 14:00:29 +0300

    TabLayout

--- app/src/main/java/com/denizsimsek/newseventsapp/view/MainActivity.kt
+++ app/src/main/java/com/denizsimsek/newseventsapp/view/MainActivity.kt
@@ -10,6 +10,7 @@ import com.denizsimsek.newseventsapp.model.NewsList
 import com.denizsimsek.newseventsapp.R
 import com.denizsimsek.newseventsapp.adapter.ViewPagerAdapter
 import com.denizsimsek.newseventsapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.GsonBuilder
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -19,6 +20,7 @@ import io.reactivex.schedulers.Schedulers
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
+    private var labels = arrayListOf<String>("Haberler","Etkinlikler")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,6 +33,12 @@ class MainActivity : AppCompatActivity() {
 
         val adapter= ViewPagerAdapter(fragments,this)
         viewPager.adapter=adapter
+
+        val tabLayout=binding.tabLayout
+        TabLayoutMediator(tabLayout,viewPager){tab,position->
+            tab.text=labels[position].toString()
+
+        }.attach()
 
 
 
